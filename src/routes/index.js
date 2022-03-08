c1f8645b036d80de0b804624c3c915586c760255
unknown <subhanhanifa@gmail.com>
2022-03-08 16:34:55 +0700

    Create addStory.js for adding new story

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,18 +4,15 @@ const router = express.Router()
 const { auth } = require('../middlewares/auth')
 
 const { register, login } = require('../controllers/auth')
-
-
-
-
-
-
-
+const { uploadFile } = require('../middlewares/uploadFile')
+const { addStory } = require('../controllers/story')
 
 
 
 router.post('/register', register)
 router.post('/login', login)
+
+router.post('/story', auth, uploadFile("image"), addStory)
 
 
 module.exports = router
